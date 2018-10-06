802227fc9b94b31940ddfe01b8ba50c2788ef12a
spitfire35 <anbrigidi@gmail.com>
2018-10-06 13:57:25 -0400

    Update Layout/Layout.ino

--- Layout/Layout.ino
+++ Layout/Layout.ino
@@ -1,6 +1,12 @@
+// Declare variables for the Pin numbers of the sensors here
+
+// EX: const int var1 = (pin #);
+
 void setup() {
   // put your setup code here, to run once:
   // setup the sensors and their pin numbers
+  
+  // EX: pinMode(var1, output)
 }
 
 void loop() {
