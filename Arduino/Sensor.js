1b38ec3a5278f9d0246419bf4e0f0fb23514708f
spitfire35 <anbrigidi@gmail.com>
2018-10-06 18:19:38 -0400

    Update Arduino/Sensor.js

--- Arduino/Sensor.js
+++ Arduino/Sensor.js
@@ -1,10 +1,7 @@
-var five = require("johnny-five"),
-    board = new five.Board();
-
-board.on("ready", function() {
-  // Create an Led on pin 13
-  var led = new five.Led(13);
-
-  // Strobe the pin on/off, defaults to 100ms phases
-  led.strobe();
+var five = require('johnny-five');
+var board = new five.Board();
+ 
+board.on('ready', function() {
+  var led = new five.Led(13); // pin 13
+  led.blink(500); // 500ms interval
 });
