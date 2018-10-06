583e6dfa8753f6634a286995ea6427758ef8cd5f
spitfire35 <anbrigidi@gmail.com>
2018-10-06 13:54:38 -0400

    Update Layout/Layout.ino

---
+++ Layout/Layout.ino
@@ -0,0 +1,9 @@
+void setup() {
+  // put your setup code here, to run once:
+  // setup the sensors and their pin numbers
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  // How the sensors will continually check and update the Admin and the Client
+}
