db9e770ac2dee7c92caa57e8aeb64894113c6e01
Colin Arscott <carscott@optonline.net>
2018-10-06 19:01:40 -0400

    Revert "Added the Firmware File for Arduino Uno"
    This reverts commit 14902a2ac9b0223e18adac0fb201f431ff57b2bc.

--- website.js
+++ website.js
@@ -1,6 +1,9 @@
 var http = require('http');
+var console = require('console')
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello World!');
+    res.end('Ur moms a hoe');
 }).listen(8080);
+
+console.log("Hello World")
